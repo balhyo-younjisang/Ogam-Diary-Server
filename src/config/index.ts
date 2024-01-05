1e5df1c51270eb7c7d0c976f2c5809dda04385ac
unknown <yjs12180825@gmail.com>
2024-01-06 01:21:58 +0900

    :tada: 프로젝트 시작

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+// "development"를 NODE_ENV 기본값으로 설정
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const foundedEnv = dotenv.config();
+if (foundedEnv.error) {
+  throw new Error("⚠️ Couldn't find .env file ⚠️");
+}
+
+export default {
+  port: parseInt(process.env.PORT!, 3000),
+  logs: {
+    level: process.env.LOG_LEVEL || "silly",
+  },
+};
