f53102675dabd61d8ffe2059ec96bf40ed668e0d
unknown <yjs12180825@gmail.com>
2024-01-14 06:14:25 +0900

    :heavy_plus_sign: 의존성 주입을 위한 typedi 종속성 추가 :heavy_plus_sign:

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,10 +1,43 @@
 import { Router, Request, Response } from "express";
+import { celebrate, Joi } from "celebrate";
 
 const route = Router();
 
 export default (app: Router) => {
   app.use("/users", route);
 
-  route.post("/login", (req: Request, res: Response) => {});
-  route.post("/join", (req: Request, res: Response) => {});
+  /***
+   * @path /api/v1/login
+   * @description email과 password를 json 형태로 전달받아 로그인 로직 처리
+   */
+  route.post(
+    "/login",
+    celebrate({
+      body: Joi.object({
+        email: Joi.string()
+          .required()
+          .regex(new RegExp("/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/")), // 이메일 유효성 검사
+        password: Joi.string().required(),
+      }),
+    }),
+    (req: Request, res: Response) => {}
+  );
+
+  /***
+   * @path /api/v1/join
+   * @description email과 password, confirm password 를 json 형태로 전달받아 회원가입 로직 처리
+   */
+  route.post(
+    "/join",
+    celebrate({
+      body: Joi.object({
+        email: Joi.string()
+          .required()
+          .regex(new RegExp("/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/")), // 이메일 유효성 검사
+        password: Joi.string().required(),
+        confirmPassword: Joi.string().required(),
+      }),
+    }),
+    (req: Request, res: Response) => {}
+  );
 };
