72eff7ecf9c23948d545990648e31d9119b92375
unknown <yjs12180825@gmail.com>
2024-01-18 00:52:00 +0900

    pm2 remove

---
+++ build/app.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const config_1 = __importDefault(require("./config"));
+const loaders_1 = __importDefault(require("./loaders"));
+const logger_1 = __importDefault(require("./loaders/logger"));
+async function startServer() {
+    const app = (0, express_1.default)();
+    (0, loaders_1.default)({ experssApp: app });
+    app
+        .listen(config_1.default.port, () => {
+        logger_1.default.info(`
+      ################################################
+      🛡️  Server listening on port: ${config_1.default.port} 🛡️
+      ################################################
+    `);
+    })
+        .on("error", (err) => {
+        process.exit(1);
+    });
+}
+startServer();
+//# sourceMappingURL=app.js.map
