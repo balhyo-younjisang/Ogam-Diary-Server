54c84e6b55472281cf230e98003b9e2e611895f2
unknown <yjs12180825@gmail.com>
2024-01-17 10:19:46 +0900

    env 파일 수정

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 // "development"를 NODE_ENV 기본값으로 설정
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const foundedEnv = dotenv.config();
-if (foundedEnv.error) {
-  throw new Error("⚠️ Couldn't find .env file ⚠️");
-}
+// const foundedEnv = dotenv.config();
+// if (foundedEnv.error) {
+//   throw new Error("⚠️ Couldn't find .env file ⚠️");
+// }
 
 export default {
   port: parseInt(process.env.PORT!, 10),
